ba2703bed9d859869b34253e454daf9d539a001e
manojmsr <71551329+manojmsr@users.noreply.github.com>
2020-09-19 18:33:19 +0530

    Add files via upload

---
+++ persinfo/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Personal Info';
+  name = ' Manoj Singh';
+  reg =' 201800135';
+  email = ' manojsinghmsr2000@gmail.com'
+  about = " I'm a computer science student currently studying in SMIT!";
+}
